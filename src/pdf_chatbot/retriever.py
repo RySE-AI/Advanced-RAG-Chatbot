7b0af5ea5fde8967b47232061275445910368084
RySE-AI <Ryo.Shibata@outlook.de>
2024-04-16 13:52:09 +0200

    Feat: Adapt MultiqueryRetriever to return queries

---
+++ src/pdf_chatbot/retriever.py
@@ -0,0 +1,130 @@
+import asyncio
+import logging
+from typing import List, Optional, Sequence
+
+from langchain_core.callbacks import (
+    AsyncCallbackManagerForRetrieverRun,
+    CallbackManagerForRetrieverRun,
+)
+from langchain_core.documents import Document
+from langchain_core.language_models import BaseLanguageModel
+from langchain_core.output_parsers import BaseOutputParser
+from langchain_core.prompts.prompt import PromptTemplate
+from langchain_core.retrievers import BaseRetriever
+from langchain.retrievers.multi_query import MultiQueryRetriever
+
+from langchain.chains.llm import LLMChain
+
+logger = logging.getLogger(__name__)
+
+
+class LineListOutputParser(BaseOutputParser[List[str]]):
+    """Output parser for a list of lines."""
+
+    def parse(self, text: str) -> List[str]:
+        lines = text.strip().split("\n")
+        return lines
+
+
+# Default prompt
+DEFAULT_QUERY_PROMPT = PromptTemplate(
+    input_variables=["question"],
+    template="""You are an AI language model assistant. Your task is 
+    to generate 3 different versions of the given user 
+    question to retrieve relevant documents from a vector database.
+    All versions must be written in german.
+    By generating multiple perspectives on the user question, 
+    your goal is to help the user overcome some of the limitations 
+    of distance-based similarity search. Provide these alternative 
+    questions separated by newlines. Original question: {question}""",
+)
+
+    
+class MultiQueryRetrieverWithQueries(MultiQueryRetriever):
+    return_queries: bool = False
+        
+    @classmethod
+    def from_llm(
+        cls,
+        retriever: BaseRetriever,
+        llm: BaseLanguageModel,
+        prompt: PromptTemplate = DEFAULT_QUERY_PROMPT,
+        parser_key: Optional[str] = None,
+        include_original: bool = False,
+        return_queries: bool = False
+    ) -> "MultiQueryRetriever":
+        """Initialize from llm using default template.
+
+        Args:
+            retriever: retriever to query documents from
+            llm: llm for query generation using DEFAULT_QUERY_PROMPT
+            include_original: Whether to include the original query in the list of
+                generated queries.
+
+        Returns:
+            MultiQueryRetriever
+        """
+        output_parser = LineListOutputParser()
+        llm_chain = LLMChain(llm=llm, prompt=prompt, output_parser=output_parser)
+        return cls(
+            retriever=retriever,
+            llm_chain=llm_chain,
+            include_original=include_original,
+            return_queries=return_queries
+        )
+
+    async def _aget_relevant_documents(
+        self,
+        query: str,
+        *,
+        run_manager: AsyncCallbackManagerForRetrieverRun,
+    ) -> List[Document]:
+        """Get relevant documents given a user query.
+
+        Args:
+            question: user query
+
+        Returns:
+            Unique union of relevant documents from all generated queries
+        """
+        queries = await self.agenerate_queries(query, run_manager)
+        if self.include_original:
+            queries.append(query)
+        documents = await self.aretrieve_documents(queries, run_manager)
+        unique_docs = self.unique_union(documents)
+        
+        if self.return_queries:
+            results = {"documents": unique_docs,
+                       "queries": queries}
+            return results
+        
+        else:
+            return self.unique_union(documents)
+        
+    def _get_relevant_documents(
+        self,
+        query: str,
+        *,
+        run_manager: CallbackManagerForRetrieverRun,
+    ) -> List[Document]:
+        """Get relevant documents given a user query.
+
+        Args:
+            question: user query
+
+        Returns:
+            Unique union of relevant documents from all generated queries
+        """
+        queries = self.generate_queries(query, run_manager)
+        if self.include_original:
+            queries.append(query)
+        documents = self.retrieve_documents(queries, run_manager)
+        unique_docs = self.unique_union(documents)
+        
+        if self.return_queries:
+            results = {"documents": unique_docs,
+                    "queries": queries}
+            return results
+        
+        else:
+            return self.unique_union(documents)
