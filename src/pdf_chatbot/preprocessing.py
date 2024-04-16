f3eb6feefc6fc6788e2b6f8ab8e96d34807fdb1c
RySE-AI <Ryo.Shibata@outlook.de>
2024-04-16 20:50:24 +0200

    refractor: Change page variable to doc for clarity

--- src/pdf_chatbot/preprocessing.py
+++ src/pdf_chatbot/preprocessing.py
@@ -9,12 +9,12 @@ from pydantic import BaseModel
 
 class Preprocessor(BaseModel, abc.ABC):
 
-    def __call__(self, page, *args, **kwargs):
-        page = self.preprocess_page(page, *args, **kwargs)
-        return page
+    def __call__(self, doc, *args, **kwargs):
+        doc = self.preprocess_page(doc, *args, **kwargs)
+        return doc
     
     @abc.abstractmethod
-    def preprocess_page(self, page, *args, **kwargs):
+    def preprocess_page(self, doc, *args, **kwargs):
         ...       
 
 
@@ -29,34 +29,34 @@ class PreprocessorComposer(BaseModel):
 
 
 class RemoveHeader(Preprocessor):
-    def preprocess_page(self, page, *args, **kwargs):
-        if "rm_header" not in page.metadata:
-            text = page.page_content
+    def preprocess_page(self, doc, *args, **kwargs):
+        if "rm_header" not in doc.metadata:
+            text = doc.page_content
             content = text.split("\n", 1)[1] #usually the first newline is the top line 
-            page.page_content = content
-            page.metadata.update(rm_header=True)
+            doc.page_content = content
+            doc.metadata.update(rm_header=True)
             
-        return page
+        return doc
     
 
 class SimpleDehyphens(Preprocessor):
-    def preprocess_page(self, page, *args, **kwargs):
-        text = page.page_content
+    def preprocess_page(self, doc, *args, **kwargs):
+        text = doc.page_content
         content = text.replace(" -\n", "") # not optimal! 
-        page.page_content = content
-        return page
+        doc.page_content = content
+        return doc
     
     
 class RemoveStartingNumbers(Preprocessor):
-    def preprocess_page(self, page, *args, **kwargs):
+    def preprocess_page(self, doc, *args, **kwargs):
         #TODO Do not remove sections numbers at the beginning
-        text = page.page_content
+        text = doc.page_content
         # used gpt for the regex....looks a bit weird
         cleaned_string = re.sub(r'^(\d{3,}\.?\d*\s|\d{1,2}(?=\s)(?!\.\d+))', '', text)
         # Adjust to ensure non-valid single and double-digit numbers followed directly by letters are removed
-        page.page_content = re.sub(r'^(\d{1,2})(?=[^\d\s\.])', '', cleaned_string)
+        doc.page_content = re.sub(r'^(\d{1,2})(?=[^\d\s\.])', '', cleaned_string)
         
-        return page
+        return doc
     
     
 class AddSectionNamesWithTOC(Preprocessor):
@@ -83,15 +83,15 @@ class AddSectionNamesWithTOC(Preprocessor):
         title = sections[index, 1]
         return title
     
-    def preprocess_page(self, page, *args, **kwargs):
-        page_num = page.metadata["page"]
+    def preprocess_page(self, doc, *args, **kwargs):
+        page_num = doc.metadata["page"]
         section_title = self._find_section_from_toc(page_num)
         splitted = section_title.split(" ", 1)
 
         if re.search(r"\d", splitted[0]):
-            page.metadata["section_title"] = splitted[1]
-            page.metadata["section_number"] = splitted[0]
+            doc.metadata["section_title"] = splitted[1]
+            doc.metadata["section_number"] = splitted[0]
         else:
-            page.metadata["section_title"] = section_title
+            doc.metadata["section_title"] = section_title
             
-        return page
+        return doc
