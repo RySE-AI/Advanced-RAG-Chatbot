5f76777294bf69ca17d7c99fdf294b4c100b20d3
RySE-AI <Ryo.Shibata@outlook.de>
2024-04-18 18:24:33 +0200

    feat: Add document preprocessing function

--- src/pdf_chatbot/utils.py
+++ src/pdf_chatbot/utils.py
@@ -1,9 +1,12 @@
 from pathlib import Path
-from typing import List
-from typing import Dict
+from typing import List, Dict
 
+import fitz
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.documents.base import Document
-import fitz  
+
+from .preprocessing import Preprocessor, PreprocessorComposer
+
 
 def get_pdf_files(dir_path: str, ext: str = "pdf") -> List[str]:
     """
@@ -11,9 +14,27 @@ def get_pdf_files(dir_path: str, ext: str = "pdf") -> List[str]:
     using glob and pathlib.
     """
     base_path = Path(dir_path)
-    pdf_files = list(base_path.rglob(f'*.{ext}'))
+    pdf_files = list(base_path.rglob(f"*.{ext}"))
     pdf_file_paths = [str(pdf) for pdf in pdf_files]
     return pdf_file_paths
+
+
+def preprocess_pdf(
+    file_path: str,
+    preprocess: PreprocessorComposer | Preprocessor,
+    start_page: int = None,
+    end_page: int = None,
+    add_meta: dict = {},
+):
+
+    loader = PyPDFLoader(file_path)
+    pages = loader.load_and_split()[start_page:end_page]
+
+    for page in pages:
+        page.metadata.update(add_meta)
+        _ = preprocess(page, add_meta)
+
+    return pages
 
 
 def simple_format_docs(docs):
